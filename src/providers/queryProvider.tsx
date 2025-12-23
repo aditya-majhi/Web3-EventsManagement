091ebe87142c16c88f0109d695c588dafc25500f
Aditya Majhi <majhi2723@gmail.com>
2025-12-24 01:37:37 +0530

    Fixed react-query version issue

--- src/providers/queryProvider.tsx
+++ src/providers/queryProvider.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
-    return (
-        <QueryClientProvider client= { queryClient } >
-        { children }
-        </QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
