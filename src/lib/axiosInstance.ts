b8ea2a9e297173647cc33c039e0e85bfc0bc87f4
Aditya Majhi <majhi2723@gmail.com>
2025-12-24 12:51:09 +0530

    Fixed CORS

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_BASE = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_URL : "http://localhost:3000";
 
 export const axiosInstance = axios.create({
     baseURL: API_BASE,
